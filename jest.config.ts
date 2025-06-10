626bde007cfa6e6ec6a503b80f09111098c88761
Lucas Vieira <lucas_99silva@hotmail.com>
2025-06-09 23:46:27 -0300

    refactor: move folders to presentation

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,8 @@ const config: Config = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
+    '^@tests/(.*)$': '<rootDir>/tests/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.svg$': '<rootDir>/svg.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
