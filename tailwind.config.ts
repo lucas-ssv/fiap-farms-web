3f6985eb335fbb3f6042de1ac814145be56befd3
Lucas Vieira <lucas_99silva@hotmail.com>
2025-06-11 13:13:25 -0300

    style: configure fonts and assets

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['var(--font-family-body)'],
+      },
+      fontWeight: {
+        regular: 'var(--font-weight-regular)',
+        medium: 'var(--font-weight-medium)',
+        semibold: 'var(--font-weight-semibold)',
+        bold: 'var(--font-weight-bold)',
+      }
+    },
+  },
+  plugins: [],
+}
+
+export default config
