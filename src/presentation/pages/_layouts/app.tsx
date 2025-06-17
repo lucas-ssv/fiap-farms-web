a74bcfc88e578c50c332a60ffe15e9c0914e3668
Lucas Vieira <lucas_99silva@hotmail.com>
2025-06-17 00:20:35 -0300

    feat: add DataProductsTable component

--- src/presentation/pages/_layouts/app.tsx
+++ src/presentation/pages/_layouts/app.tsx
@@ -46,7 +46,7 @@ export function AppLayout() {
             <Bell />
           </Button>
         </header>
-        <div className="flex flex-1 flex-col gap-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 pt-0 mb-20">
           <Outlet />
         </div>
       </SidebarInset>
