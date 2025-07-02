383a7479ed6635b8c1c86dec4fc16a356656e15b
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-02 17:23:20 -0300

    feat: add AddProduct usecase

---
+++ src/domain/usecases/product/add-product.ts
@@ -0,0 +1,21 @@
+export interface AddProduct {
+  execute: (data: AddProduct.Params) => Promise<void>
+}
+
+export namespace AddProduct {
+  export type Params = {
+    id: string
+    name: string
+    price: number
+    cost: number
+    categoryId: string
+    stock: number
+    minStock: number
+    maxStock: number
+    unit: string
+    description: string
+    image: string
+    createdAt: Date
+    updatedAt: Date
+  }
+}
