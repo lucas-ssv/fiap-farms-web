9537452f7b75289c112e09d2a2524c123e619ccb
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-03 11:48:50 -0300

    feat: ensure UploadFirebaseService calls ref with correct values

--- src/infra/services/firebase/upload-firebase-service.ts
+++ src/infra/services/firebase/upload-firebase-service.ts
@@ -1,9 +1,12 @@
 import type { UploadService } from '@/data/contracts/services'
+import { storage } from '@/main/config/firebase'
+import { ref } from 'firebase/storage'
 import { v4 as uuidv4 } from 'uuid'
 
 export class UploadFirebaseService implements UploadService {
   async upload(uri: string): Promise<UploadService.Result> {
-    uuidv4()
+    const fileName = uuidv4()
+    const storageRef = ref(storage, `products/${fileName}`)
     return Promise.resolve(null as any)
   }
 }
