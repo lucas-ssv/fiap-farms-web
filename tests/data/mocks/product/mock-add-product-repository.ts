00324b2c3addad73a8d6065e7d3f855b126d02c5
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-03 10:09:35 -0300

    test: ensure AddProduct returns product id on success

--- tests/data/mocks/product/mock-add-product-repository.ts
+++ tests/data/mocks/product/mock-add-product-repository.ts
@@ -1,5 +1,9 @@
 import type { AddProductRepository } from '@/data/contracts/product'
 
 export class AddProductRepositoryMock implements AddProductRepository {
-  async add(data: AddProductRepository.Params): Promise<void> {}
+  async add(
+    data: AddProductRepository.Params
+  ): Promise<AddProductRepository.ProductId> {
+    return 'any_product_id'
+  }
 }
