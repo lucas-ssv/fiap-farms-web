6bd636eade159bf89704d17f55ef6f79a1dab9e0
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-03 11:52:31 -0300

    feat: ensure UploadFirebaseService calls uriToBlob with correct uri

--- src/infra/services/firebase/upload-firebase-service.ts
+++ src/infra/services/firebase/upload-firebase-service.ts
@@ -1,4 +1,5 @@
 import type { UploadService } from '@/data/contracts/services'
+import { uriToBlob } from '@/infra/utils'
 import { storage } from '@/main/config/firebase'
 import { ref } from 'firebase/storage'
 import { v4 as uuidv4 } from 'uuid'
@@ -7,6 +8,7 @@ export class UploadFirebaseService implements UploadService {
   async upload(uri: string): Promise<UploadService.Result> {
     const fileName = uuidv4()
     const storageRef = ref(storage, `products/${fileName}`)
+    const blob = await uriToBlob(uri)
     return Promise.resolve(null as any)
   }
 }
