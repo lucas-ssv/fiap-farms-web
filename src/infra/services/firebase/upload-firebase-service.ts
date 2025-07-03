96b5e58caf10aa0009a495de0907c9fa7615daea
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-03 11:56:03 -0300

    feat: ensure UploadFirebaseService calls uploadBytesResumable with correct values

--- src/infra/services/firebase/upload-firebase-service.ts
+++ src/infra/services/firebase/upload-firebase-service.ts
@@ -1,7 +1,7 @@
 import type { UploadService } from '@/data/contracts/services'
 import { uriToBlob } from '@/infra/utils'
 import { storage } from '@/main/config/firebase'
-import { ref } from 'firebase/storage'
+import { ref, uploadBytesResumable } from 'firebase/storage'
 import { v4 as uuidv4 } from 'uuid'
 
 export class UploadFirebaseService implements UploadService {
@@ -9,6 +9,7 @@ export class UploadFirebaseService implements UploadService {
     const fileName = uuidv4()
     const storageRef = ref(storage, `products/${fileName}`)
     const blob = await uriToBlob(uri)
+    const uploadTask = uploadBytesResumable(storageRef, blob)
     return Promise.resolve(null as any)
   }
 }
