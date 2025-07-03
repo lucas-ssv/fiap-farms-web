75a38ace44356f8127d72c44ffd5754cf4b527c8
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-03 14:41:52 -0300

    test: ensure AddProduct calls UploadService with correct values

--- tests/data/usecases/product/add-product.spec.ts
+++ tests/data/usecases/product/add-product.spec.ts
@@ -2,18 +2,31 @@ import { AddProductImpl } from '@/data/usecases/product'
 import type { AddProduct } from '@/domain/usecases/product'
 import { AddProductRepositoryMock } from '@tests/data/mocks/product'
 import { mockAddProductParams } from './mocks'
+import type { UploadService } from '@/data/contracts/services'
+
+class UploadServiceMock implements UploadService {
+  async upload(uri: string): Promise<UploadService.Result> {
+    return {
+      name: 'any_name',
+      url: 'any_url',
+    }
+  }
+}
 
 type SutTypes = {
   sut: AddProduct
   addProductRepositoryMock: AddProductRepositoryMock
+  uploadServiceMock: UploadServiceMock
 }
 
 const makeSut = (): SutTypes => {
   const addProductRepositoryMock = new AddProductRepositoryMock()
-  const sut = new AddProductImpl(addProductRepositoryMock)
+  const uploadServiceMock = new UploadServiceMock()
+  const sut = new AddProductImpl(addProductRepositoryMock, uploadServiceMock)
   return {
     sut,
     addProductRepositoryMock,
+    uploadServiceMock,
   }
 }
 
@@ -26,6 +39,16 @@ describe('AddProduct usecase', () => {
     await sut.execute(params)
 
     expect(addSpy).toHaveBeenCalledWith(params)
+  })
+
+  it('should call UploadService with correct values', async () => {
+    const { sut, uploadServiceMock } = makeSut()
+    const uploadSpy = jest.spyOn(uploadServiceMock, 'upload')
+    const params = mockAddProductParams()
+
+    await sut.execute(params)
+
+    expect(uploadSpy).toHaveBeenCalledWith(params.image)
   })
 
   it('should throw if AddProductRepository throws', async () => {
