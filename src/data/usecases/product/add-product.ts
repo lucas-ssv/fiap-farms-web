ef578e1981f62ec44ee6827fcf06c2b4b6527240
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-03 14:48:16 -0300

    feat: ensure AddProduct calls UpdateProductRepository with correct values

--- src/data/usecases/product/add-product.ts
+++ src/data/usecases/product/add-product.ts
@@ -1,21 +1,30 @@
-import type { AddProductRepository } from '@/data/contracts/product'
+import type {
+  AddProductRepository,
+  UpdateProductRepository,
+} from '@/data/contracts/product'
 import type { UploadService } from '@/data/contracts/services'
 import type { AddProduct } from '@/domain/usecases/product'
 
 export class AddProductImpl implements AddProduct {
   private addProductRepository: AddProductRepository
   private uploadService: UploadService
+  private updateProductRepository: UpdateProductRepository
 
   constructor(
     addProductRepository: AddProductRepository,
-    uploadService: UploadService
+    uploadService: UploadService,
+    updateProductRepository: UpdateProductRepository
   ) {
     this.addProductRepository = addProductRepository
     this.uploadService = uploadService
+    this.updateProductRepository = updateProductRepository
   }
 
   async execute(data: AddProduct.Params): Promise<void> {
-    await this.addProductRepository.add(data)
-    await this.uploadService.upload(data.image)
+    const productId = await this.addProductRepository.add(data)
+    const { url } = await this.uploadService.upload(data.image)
+    await this.updateProductRepository.update(productId, {
+      image: url,
+    })
   }
 }
