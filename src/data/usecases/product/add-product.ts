11aae9589899c182e74809aa57f3d6b8a164c5e3
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-03 14:38:15 -0300

    feat: ensure AddProduct calls UploadService with correct values

--- src/data/usecases/product/add-product.ts
+++ src/data/usecases/product/add-product.ts
@@ -1,14 +1,21 @@
 import type { AddProductRepository } from '@/data/contracts/product'
+import type { UploadService } from '@/data/contracts/services'
 import type { AddProduct } from '@/domain/usecases/product'
 
 export class AddProductImpl implements AddProduct {
-  private readonly addProductRepository: AddProductRepository
+  private addProductRepository: AddProductRepository
+  private uploadService: UploadService
 
-  constructor(addProductRepository: AddProductRepository) {
+  constructor(
+    addProductRepository: AddProductRepository,
+    uploadService: UploadService
+  ) {
     this.addProductRepository = addProductRepository
+    this.uploadService = uploadService
   }
 
   async execute(data: AddProduct.Params): Promise<void> {
     await this.addProductRepository.add(data)
+    await this.uploadService.upload(data.image)
   }
 }
