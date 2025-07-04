12202d4a90c81e8f3889f3cf6ad66ae2a4ab4494
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-04 09:55:22 -0300

    test: ensure AddCategory calls UploadService with correct values

--- tests/data/usecases/category/add-category.spec.ts
+++ tests/data/usecases/category/add-category.spec.ts
@@ -1,17 +1,21 @@
 import { AddCategoryImpl } from '@/data/usecases/category'
 import { AddCategoryRepositoryMock } from '@tests/data/mocks/category'
+import { UploadServiceMock } from '@tests/data/mocks/services'
 
 type SutTypes = {
   sut: AddCategoryImpl
   addCategoryRepositoryMock: AddCategoryRepositoryMock
+  uploadServiceMock: UploadServiceMock
 }
 
 const makeSut = (): SutTypes => {
   const addCategoryRepositoryMock = new AddCategoryRepositoryMock()
-  const sut = new AddCategoryImpl(addCategoryRepositoryMock)
+  const uploadServiceMock = new UploadServiceMock()
+  const sut = new AddCategoryImpl(addCategoryRepositoryMock, uploadServiceMock)
   return {
     sut,
     addCategoryRepositoryMock,
+    uploadServiceMock,
   }
 }
 
@@ -31,5 +35,19 @@ describe('AddCategory', () => {
       name: data.name,
       description: data.description,
     })
+  })
+
+  it('should call UploadService with correct values', async () => {
+    const { sut, uploadServiceMock } = makeSut()
+    const uploadSpy = jest.spyOn(uploadServiceMock, 'upload')
+    const data = {
+      name: 'any_name',
+      description: 'any_description',
+      image: new File([''], 'any_image.png', { type: 'image/png' }),
+    }
+
+    await sut.execute(data)
+
+    expect(uploadSpy).toHaveBeenCalledWith(data.image)
   })
 })
