4c982edc036c3952fa9f5d8dab05b003335967ac
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-04 09:54:54 -0300

    feat: ensure AddCategory calls UploadService with correct values

--- src/data/usecases/category/add-category.ts
+++ src/data/usecases/category/add-category.ts
@@ -1,15 +1,22 @@
 import type { AddCategoryRepository } from '@/data/contracts/category'
+import type { UploadService } from '@/data/contracts/services'
 import type { AddCategory } from '@/domain/usecases/category'
 
 export class AddCategoryImpl implements AddCategory {
   private addCategoryRepository: AddCategoryRepository
+  private uploadService: UploadService
 
-  constructor(addCategoryRepository: AddCategoryRepository) {
+  constructor(
+    addCategoryRepository: AddCategoryRepository,
+    uploadService: UploadService
+  ) {
     this.addCategoryRepository = addCategoryRepository
+    this.uploadService = uploadService
   }
 
   async execute(data: AddCategory.Params): Promise<void> {
     const { image, ...dataWithoutImage } = data
     await this.addCategoryRepository.add(dataWithoutImage)
+    await this.uploadService.upload(image!)
   }
 }
