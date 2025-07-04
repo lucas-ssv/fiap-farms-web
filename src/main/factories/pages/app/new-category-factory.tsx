44bab691119100aa4fdb579c6c1f1944b98a9d61
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-04 11:23:44 -0300

    feat: add NewCategory composition

--- src/main/factories/pages/app/new-category-factory.tsx
+++ src/main/factories/pages/app/new-category-factory.tsx
@@ -1,7 +1,15 @@
 import { AddCategoryImpl } from '@/data/usecases/category'
+import { CategoryFirebaseRepository } from '@/infra/repositories/firebase/category'
+import { UploadFirebaseService } from '@/infra/services/firebase'
 import { NewCategory } from '@/presentation/pages/app/Products'
 
 export function MakeNewCategory() {
-  const addCategory = new AddCategoryImpl()
-  return <NewCategory addCategory={} />
+  const categoryFirebaseRepository = new CategoryFirebaseRepository()
+  const uploadService = new UploadFirebaseService()
+  const addCategory = new AddCategoryImpl(
+    categoryFirebaseRepository,
+    uploadService,
+    categoryFirebaseRepository
+  )
+  return <NewCategory addCategory={addCategory} />
 }
