def7ba0da19099e46229182b830de1f2ffe5489e
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-04 11:52:57 -0300

    feat: add LoadCategories usecase

---
+++ src/domain/usecases/category/load-categories.ts
@@ -0,0 +1,12 @@
+export interface LoadCategories {
+  execute: () => Promise<LoadCategories.Result>
+}
+
+export namespace LoadCategories {
+  export type Result = Array<{
+    id: string
+    name: string
+    description?: string
+    image?: string
+  }>
+}
