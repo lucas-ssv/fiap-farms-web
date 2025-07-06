e177d9bb39fa0301b8ded132dd0a5424909cf25f
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-06 10:41:44 -0300

    refactor: move sut creation to a factory method

--- tests/data/usecases/product/load-products.spec.ts
+++ tests/data/usecases/product/load-products.spec.ts
@@ -1,11 +1,24 @@
 import { LoadProductsImpl } from '@/data/usecases/product'
 import { LoadProductsRepositoryStub } from '@tests/data/mocks/product'
 
+type SutTypes = {
+  sut: LoadProductsImpl
+  loadProductsRepositoryStub: LoadProductsRepositoryStub
+}
+
+const makeSut = (): SutTypes => {
+  const loadProductsRepositoryStub = new LoadProductsRepositoryStub()
+  const sut = new LoadProductsImpl(loadProductsRepositoryStub)
+  return {
+    sut,
+    loadProductsRepositoryStub,
+  }
+}
+
 describe('LoadProducts usecase', () => {
   it('should call LoadProductsRepository with correct values', async () => {
-    const loadProductsRepositoryStub = new LoadProductsRepositoryStub()
+    const { sut, loadProductsRepositoryStub } = makeSut()
     const loadAllSpy = jest.spyOn(loadProductsRepositoryStub, 'loadAll')
-    const sut = new LoadProductsImpl(loadProductsRepositoryStub)
 
     await sut.execute()
 
