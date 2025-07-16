7a71ad94b866f8b2fe4075a45500c39fe31bb71d
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-16 12:14:37 -0300

    fix: adjust schema validation

--- src/main/factories/pages/app/Products/new-product-factory.tsx
+++ src/main/factories/pages/app/Products/new-product-factory.tsx
@@ -2,16 +2,19 @@ import { LoadCategoriesImpl } from '@/data/usecases/category'
 import { AddProductImpl } from '@/data/usecases/product'
 import { CategoryFirebaseRepository } from '@/infra/repositories/firebase/category'
 import { ProductFirebaseRepository } from '@/infra/repositories/firebase/product'
+import { StockMovementFirebaseRepository } from '@/infra/repositories/firebase/stock-movement'
 import { UploadFirebaseService } from '@/infra/services/firebase'
 import { NewProduct } from '@/presentation/pages/app/Products'
 
 export function MakeNewProduct() {
   const productFirebaseRepository = new ProductFirebaseRepository()
   const uploadService = new UploadFirebaseService()
+  const stockMovementFirebaseRepository = new StockMovementFirebaseRepository()
   const addProduct = new AddProductImpl(
     productFirebaseRepository,
     uploadService,
-    productFirebaseRepository
+    productFirebaseRepository,
+    stockMovementFirebaseRepository
   )
   const categoryFirebaseRepository = new CategoryFirebaseRepository()
   const loadCategories = new LoadCategoriesImpl(categoryFirebaseRepository)
