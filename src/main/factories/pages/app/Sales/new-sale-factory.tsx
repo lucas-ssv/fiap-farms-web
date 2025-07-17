89935ffc389169d31ffbc2f98d2802782ac84eaa
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-17 13:52:29 -0300

    feat: add stockMovementFirebaseRepository on NewSale composition

--- src/main/factories/pages/app/Sales/new-sale-factory.tsx
+++ src/main/factories/pages/app/Sales/new-sale-factory.tsx
@@ -4,6 +4,7 @@ import { AddSaleImpl } from '@/data/usecases/sale'
 import { CustomerFirebaseRepository } from '@/infra/repositories/firebase/customer'
 import { ProductFirebaseRepository } from '@/infra/repositories/firebase/product'
 import { SaleFirebaseRepository } from '@/infra/repositories/firebase/sale'
+import { StockMovementFirebaseRepository } from '@/infra/repositories/firebase/stock-movement'
 import { NewSale } from '@/presentation/pages/app/Sales'
 
 export function MakeNewSale() {
@@ -12,7 +13,11 @@ export function MakeNewSale() {
   const customerFirebaseRepository = new CustomerFirebaseRepository()
   const loadCustomers = new LoadCustomersImpl(customerFirebaseRepository)
   const saleFirebaseRepository = new SaleFirebaseRepository()
-  const addSale = new AddSaleImpl(saleFirebaseRepository)
+  const stockMovementFirebaseRepository = new StockMovementFirebaseRepository()
+  const addSale = new AddSaleImpl(
+    saleFirebaseRepository,
+    stockMovementFirebaseRepository
+  )
   return (
     <NewSale
       loadProducts={loadProducts}
