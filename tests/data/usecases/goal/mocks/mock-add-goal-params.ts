2ec8de39c8e272d2b491f9591f29a3679038c7d1
Lucas Vieira <lucas_99silva@hotmail.com>
2025-07-17 14:06:06 -0300

    feat: add userId to AddGoal usecase params

--- tests/data/usecases/goal/mocks/mock-add-goal-params.ts
+++ tests/data/usecases/goal/mocks/mock-add-goal-params.ts
@@ -2,6 +2,7 @@ import type { AddGoal } from '@/domain/usecases/goal'
 
 export const mockAddGoalParams = (): AddGoal.Params => ({
   productId: 'any_product_id',
+  userId: 'any_user_id',
   description: 'any_description',
   targetValue: 200,
   currentValue: 100,
